bcb4fb5cea4b3e37fdfd7e2117829337aea6805d
kosu27 <kosumon27@gmail.com>
2021-09-27 19:28:23 +0900

    👽️ APIを叩いた

--- functions/routers/messages/route.js
+++ functions/routers/messages/route.js
@@ -1,35 +1,105 @@
 const express = require("express");
+const admin = require("firebase-admin");
+const serviceAccount = require("../../serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://test4-a29fa.firebaseio.com",
+});
 
 const router = express.Router();
 
 const endPoint = "/messages";
 
+const db = admin.firestore();
+
 // messages
 router
   .route(endPoint)
-  .get((req, res) => {
+  .get(async (req, res) => {
+    const messages = [];
+    try {
+      const querySnapshot = await db
+        .collection("messages")
+        .orderBy("createdAt", "desc")
+        .get();
+      querySnapshot.forEach((doc) => {
+        messages.push({
+          id: doc.id,
+          ...doc.data(),
+        });
+      });
+    } catch (error) {
+      console.error(error, "@@@@@@@@@");
+    }
+
     res.json({
       message: "Called by the GET method",
+      messages,
     });
   })
-  .post((req, res) => {
-    res.json({
-      message: "Called by the POST method",
-    });
+  .post(async (req, res) => {
+    const { name, body } = req.body;
+    const createdAt = new Date().toISOString();
+
+    try {
+      const docRef = await db.collection("messages").add({
+        name,
+        body,
+        createdAt,
+      });
+
+      const docSnapshot = await docRef.get();
+      const createdMessage = {
+        id: docSnapshot.id,
+        ...docSnapshot.data(),
+      };
+
+      res.json({
+        message: "Called by the POST method",
+        data: createdMessage,
+      });
+    } catch (error) {}
   });
 
 //messages/1
 router
   .route(`${endPoint}/:id`)
-  .put((req, res) => {
-    res.json({
-      message: `Called by the PUT method. ID: ${req.params.id}`,
-    });
+  .put(async (req, res) => {
+    const { id } = req.params;
+    const { name, body } = req.body;
+
+    const newData = {
+      name,
+      body,
+    };
+
+    try {
+      await db.collection("messages").doc(id).update(newData);
+
+      res.json({
+        message: `Updated!! ID: ${id}`,
+      });
+    } catch (error) {
+      res.status(500).json({
+        message: `SomethingError!! ID: ${id}`,
+      });
+    }
   })
-  .delete((req, res) => {
-    res.json({
-      message: `Called by the DELETE method. ID: ${req.params.id}`,
-    });
+  .delete(async (req, res) => {
+    const { id } = req.params;
+
+    try {
+      await db.collection("messages").doc(id).delete();
+
+      res.json({
+        message: `Deleted!! ID: ${id}`,
+      });
+    } catch (error) {
+      res.status(500).json({
+        message: `SomethingError!! ID: ${id}`,
+      });
+    }
   });
 
 module.exports = router;
